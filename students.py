d8824e554727b877d47ec95a8fba6296f1764c60
nandan <nandanshettar8197@gmail.com>
2025-09-30 09:43:17 +0530

    save

---
+++ students.py
@@ -0,0 +1,8 @@
+#program to demonstrate student details
+name=input("Enter students name:")
+USN=input("Enter students usn:")
+course=input("Enter the course name:")
+print("students details are as follows:")
+print("name:",name)
+print("usn number:",USN)
+print("course name:",course)
